08be73e99ac37c0f615a28172752edde6cc52add
Candinho Jr <candinholuiz@gmail.com>
2024-10-22 16:14:36 -0300

    Updates

---
+++ precache-manifest.f3ed7e344514f71270e7f3c5c233dc00.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9f00e8014bc20b51957a888ac649bd9",
+    "url": "/ethics-canvas/index.html"
+  },
+  {
+    "revision": "fa177a6f1bb603f1529f",
+    "url": "/ethics-canvas/static/css/2.a17ebd6e.chunk.css"
+  },
+  {
+    "revision": "45b43d8cd9cee84722af",
+    "url": "/ethics-canvas/static/css/3.764ccc25.chunk.css"
+  },
+  {
+    "revision": "c7bf8678cd26e1436c4e",
+    "url": "/ethics-canvas/static/css/main.36416e79.chunk.css"
+  },
+  {
+    "revision": "fa177a6f1bb603f1529f",
+    "url": "/ethics-canvas/static/js/2.ad9e7461.chunk.js"
+  },
+  {
+    "revision": "45b43d8cd9cee84722af",
+    "url": "/ethics-canvas/static/js/3.570a29e5.chunk.js"
+  },
+  {
+    "revision": "e0700b07cf0a189f30a5",
+    "url": "/ethics-canvas/static/js/4.48733ba7.chunk.js"
+  },
+  {
+    "revision": "c7bf8678cd26e1436c4e",
+    "url": "/ethics-canvas/static/js/main.769aabba.chunk.js"
+  },
+  {
+    "revision": "156617a98572a7abe709",
+    "url": "/ethics-canvas/static/js/runtime~main.eda5c519.js"
+  },
+  {
+    "revision": "8680cb7c543e5aae34b57fe39bf7e7d8",
+    "url": "/ethics-canvas/static/media/formatBold.8680cb7c.svg"
+  },
+  {
+    "revision": "33a744851c21d0235667b75bb9f81cd7",
+    "url": "/ethics-canvas/static/media/formatItalic.33a74485.svg"
+  },
+  {
+    "revision": "421c3226481d4bc3b7c9b2438b3a8e38",
+    "url": "/ethics-canvas/static/media/formatListBulleted.421c3226.svg"
+  },
+  {
+    "revision": "84ff2b736c3f490d52a9062e22d4020a",
+    "url": "/ethics-canvas/static/media/formatListNumbered.84ff2b73.svg"
+  },
+  {
+    "revision": "e53198a5519022b4a5146fc88b1a7677",
+    "url": "/ethics-canvas/static/media/formatSize.e53198a5.svg"
+  },
+  {
+    "revision": "ea62c122ec34200053a46df1fbc8bfb8",
+    "url": "/ethics-canvas/static/media/formatStrikethrough.ea62c122.svg"
+  },
+  {
+    "revision": "4725a735f0f9ac4902c65b97fcf11d8e",
+    "url": "/ethics-canvas/static/media/formatUnderlined.4725a735.svg"
+  },
+  {
+    "revision": "e8e466e6c688e73d59ff69a6b6b36155",
+    "url": "/ethics-canvas/static/media/note.e8e466e6.svg"
+  }
+]);
